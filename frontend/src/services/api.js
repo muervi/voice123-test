a6f0347909fd29615d95a2a77ac18a85876c4964
CarlosPure <carlos.pure@acklenavenue.com>
2025-01-31 17:04:12 -0500

    removed console log

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,7 +5,6 @@ const API_URL = 'http://localhost:5000/api/search';
 export const searchVoiceActors = async (query, page = 1) => {
     try {
         const response = await axios.get(`${API_URL}?keywords=${query}&page=${page}`);
-        console.log(response.data)
         return response.data;
     } catch (error) {
         console.error('Error fetching data:', error);
